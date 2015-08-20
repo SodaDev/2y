d7370bb426ab81f514578be0956584aabc55c4ed
SodaDev <soda@Marcins-MacBook-Pro.local>
2015-08-20 22:12:25 +0200

    Daily update

--- js/y2.js
+++ js/y2.js
@@ -2,12 +2,12 @@ var App = angular.module('y2Studio', ['ui.router']);
 
 App.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise("/main");
+    $urlRouterProvider.otherwise("/");
 
     $stateProvider
         .state('main', {
             url: "/",
-            templateUrl: "index.html"
+            template: "<y2-logo></y2-logo><y2-footer></y2-footer>"
         })
         .state('work', {
             url: "/work",
@@ -21,4 +21,43 @@ App.config(function($stateProvider, $urlRouterProvider) {
             url: "/contact",
             templateUrl: "views/contact.html"
         });
+});
+
+App.controller('WorkController', ['$scope', function($scope){
+    var vm = this;
+    vm.designs = [];
+
+    var addDesign = function(name){
+        vm.designs.push({
+            alt: name,
+            small: 'img/750/' + name + '.png',
+            big: 'img/1200/' + name + '.png'
+        })
+    };
+
+    addDesign('brod');
+    addDesign('biscuit');
+    addDesign('architecture');
+    addDesign('jorn');
+    addDesign('carpenter');
+    addDesign('biotanical');
+    addDesign('dorteRask');
+    addDesign('czarnaKawka');
+}]);
+
+App.directive('y2Logo', function(){
+    return {
+        restrict: 'E',
+        template: '<div class="row" style="margin-top: 50px">' +
+                  ' <img src="img/logo.png" class="img-responsive center-block img-logo" alt="2Y Logo">' +
+                  '     <div class="logo-text text-center"> <p>2Y GRAPHIC DESIGN STUDIO</p> </div>' +
+                  '</div>'
+    }
+});
+
+App.directive('y2Footer', function(){
+    return {
+        restrict: 'E',
+        templateUrl: 'views/footer.html'
+    }
 });
